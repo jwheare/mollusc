4dea58a90cefcc7672fb87e42f78951eb0e772fa
James Wheare <james@wheare.org>
2011-03-17 03:30:43 +0000

    No cache

--- app/controller/home.controller.php
+++ app/controller/home.controller.php
@@ -6,6 +6,8 @@ use App\Model;
 
 class HomeHtml extends Controller\Html {
     public function index () {
+        $this->session->nocache();
+        
         $e = new Model\Event;
         list($this->events, $this->totalEvents) = $e->getAll();
         return $this->response;
