d172a2ca7571293a400860b90db12e18884156c3
James Wheare <james@wheare.org>
2011-05-15 20:56:15 +0100

    Example offline url pattern

--- app/route.class.php
+++ app/route.class.php
@@ -3,8 +3,9 @@
 namespace App;
 
 class Route {
-    static function getPatterns () {
+    static function getPatterns (\Core\Request $request) {
         return array(
+            // '/.*/' => 'offline', // uncomment to put up an offline page
             '/^$/' => 'home',
         );
     }
