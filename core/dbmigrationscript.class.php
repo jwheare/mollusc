200c738c3bf0ca008f078202c88a929d7cd9ba49
James Wheare <james@wheare.org>
2012-03-11 05:17:59 +0000

    Prompt to backup first and log commands

--- core/dbmigrationscript.class.php
+++ core/dbmigrationscript.class.php
@@ -14,10 +14,17 @@ class DBMigrationScript extends Script {
         if (count($migrations)) {
             $this->out("Loading migrations:\n");
             natcasesort($migrations);
+            // Prompt
+            $this->out("\n" . implode("\n$migrationDir/", $migrations) . "\n\nAre you happy to run these SQL migrations on the `" . MYSQL_DB . "` database now?\nProbably wise to backup your database first.\n[Y/n]: ");
+            $input = strtolower(trim(fgets(STDIN)));
+            if ($input && ($input[0] == 'n' || $input == 0)) {
+                $this->end();
+            }
             foreach ($migrations as $migration) {
                 try {
-                    $this->out("$migration\n");
-                    service('db')->execute(file_get_contents("$migrationDir/$migration"));
+                    $commands = file_get_contents("$migrationDir/$migration");
+                    $this->out("\n$migration\n$commands\n");
+                    service('db')->execute($commands);
                 } catch (PDOException $e) {
                     if ($e->errorInfo[1] != 1060) { // Duplicate column
                         throw $e;
